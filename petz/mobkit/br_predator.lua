60ffda825a33ff09bdb498d7e8d9483bbb22ce91
Isidor Zeuner <minetest@quidecco.de>
2020-07-28 17:45:30 +0200

    move logic to function

--- petz/mobkit/br_predator.lua
+++ petz/mobkit/br_predator.lua
@@ -90,34 +90,9 @@ function petz.predator_brain(self)
 
 		--Roam default
 		if mobkit.is_queue_empty_high(self) and not(self.status) then
-			if not self.lay_eggs then
-                        elseif 0 == self.recovering_eggs_count then
-			else
-				local now = minetest.get_gametime(
-				)
-				if (
-					self.recovering_eggs_time + petz.settings.egg_recovery_delay
-				) < now then
-					self.recovering_eggs_count = mobkit.remember(
-						self,
-						"recovering_eggs_count",
-						self.recovering_eggs_count - 1
-					)
-					minetest.log(
-						"warning",
-						"DEBUG recovering egg count " .. self.recovering_eggs_count
-					)
-					self.recovering_eggs_time = mobkit.remember(
-						self,
-						"recovering_eggs_time",
-						now
-					)
-					minetest.log(
-						"warning",
-						"DEBUG recovering egg time " .. self.recovering_eggs_time
-					)
-				end
-			end
+			petz.bh_refill_egg(
+				self
+			)
 			mobkit.hq_roam(self, 0)
 		end
 
--- petz/mobkit/br_semiaquatic.lua
+++ petz/mobkit/br_semiaquatic.lua
@@ -81,34 +81,9 @@ function petz.semiaquatic_brain(self)
 
 		--Roam default
 		if mobkit.is_queue_empty_high(self) and not(self.status) then
-			if not self.lay_eggs then
-                        elseif 0 == self.recovering_eggs_count then
-			else
-				local now = minetest.get_gametime(
-				)
-				if (
-					self.recovering_eggs_time + petz.settings.egg_recovery_delay
-				) < now then
-					self.recovering_eggs_count = mobkit.remember(
-						self,
-						"recovering_eggs_count",
-						self.recovering_eggs_count - 1
-					)
-					minetest.log(
-						"warning",
-						"DEBUG recovering egg count " .. self.recovering_eggs_count
-					)
-					self.recovering_eggs_time = mobkit.remember(
-						self,
-						"recovering_eggs_time",
-						now
-					)
-					minetest.log(
-						"warning",
-						"DEBUG recovering egg time " .. self.recovering_eggs_time
-					)
-				end
-			end
+			petz.bh_refill_egg(
+				self
+			)
 			if petz.isinliquid(self) then
 				mobkit.hq_aqua_roam(self, 0, self.max_speed)
 			else
